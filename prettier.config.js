b9129d72525eb15014b73143564715457517f78c
Haukur Már Böðvarsson <haukurmar@gmail.com>
2019-04-04 16:55:21 +0000

    Add initialValues to useForm, Make validate function optional.

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	semi: false,
+	useTabs: true,
+	tabWidth: 4,
+	printWidth: 80,
+	singleQuote: true,
+	jsxBracketSameLine: false,
+	trailingComma: 'all',
+}
--- src/useForm.js
+++ src/useForm.js
@@ -1,34 +1,42 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
-const useForm = (callback, validate) => {
+const useForm = (initialValues, callback, validate) => {
+	const [values, setValues] = useState(initialValues)
+	const [errors, setErrors] = useState({})
+	const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const [values, setValues] = useState({});
-  const [errors, setErrors] = useState({});
-  const [isSubmitting, setIsSubmitting] = useState(false);
+	useEffect(
+		() => {
+			if (Object.keys(errors).length === 0 && isSubmitting) {
+				callback()
+			}
+		},
+		[errors],
+	)
 
-  useEffect(() => {
-    if (Object.keys(errors).length === 0 && isSubmitting) {
-      callback();
-    }
-  }, [errors]);
+	const handleSubmit = event => {
+		if (event) event.preventDefault()
+		// Only validate if the validate function is used
+		if (validate) {
+			setErrors(validate(values))
+		}
+		setIsSubmitting(true)
+	}
 
-  const handleSubmit = (event) => {
-    if (event) event.preventDefault();
-    setErrors(validate(values));
-    setIsSubmitting(true);
-  };
+	const handleChange = event => {
+		event.persist()
+		setValues(values => ({
+			...values,
+			[event.target.name]: event.target.value,
+		}))
+	}
 
-  const handleChange = (event) => {
-    event.persist();
-    setValues(values => ({ ...values, [event.target.name]: event.target.value }));
-  };
+	return {
+		handleChange,
+		handleSubmit,
+		values,
+		errors,
+	}
+}
 
-  return {
-    handleChange,
-    handleSubmit,
-    values,
-    errors,
-  }
-};
-
-export default useForm;
+export default useForm
